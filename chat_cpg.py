7164f28602c08472b72f2d2e11691fd516f18ffd
Lucas Guimarães <lucas.santosg@hotmail.com>
2025-07-03 21:20:12 -0300

    Fix import: use langchain.prompts.ChatPromptTemplate in chat_cpg.py

--- chat_cpg.py
+++ chat_cpg.py
@@ -12,7 +12,7 @@ import streamlit as st
 from dotenv import load_dotenv
 from langchain.memory import ConversationBufferMemory
 from langchain_openai import ChatOpenAI
-from langchain import ChatPromptTemplate
+from langchain.prompts import ChatPromptTemplate
 from pathlib import Path
 
 ##internal functions
