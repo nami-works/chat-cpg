a934d1d2f088ec322b84ace3098412ac449d7fcd
Lucas Guimarães <lucas.santosg@hotmail.com>
2025-07-03 22:14:27 -0300

    Force push: Update entire project

--- redacao/redacao.py
+++ redacao/redacao.py
@@ -14,7 +14,6 @@ import streamlit as st
 from dotenv import load_dotenv
 from langchain.memory import ConversationBufferMemory
 from langchain_openai import ChatOpenAI
-from langchain_groq import ChatGroq
 from langchain.prompts import ChatPromptTemplate
 
 #internal ChatCPG functions
